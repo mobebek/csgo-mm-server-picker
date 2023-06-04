e056bf93328a88497de858d93ff5739970612f4d
mobebek <91740765+mobebek@users.noreply.github.com>
2023-06-04 20:42:54 +0200

    Update servers.js
    temp fix

--- app/services/servers.js
+++ app/services/servers.js
@@ -6,7 +6,7 @@ let serversList = null;
 ServersService.prototype.getServersList = function () {
   if (serversList === null) {
     serversList = Axios({
-      url: `https://raw.githubusercontent.com/SteamDatabase/SteamTracking/master/Random/NetworkDatagramConfig.json`,
+      url: `https://raw.githubusercontent.com/SteamDatabase/SteamTracking/0ae12036fceb607d31a2cecb504f4ffa6f52d306/Random/NetworkDatagramConfig.json`,
       method: 'get'
     })
   }
